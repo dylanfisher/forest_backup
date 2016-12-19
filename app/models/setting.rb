500de4d253f5afe22c0c6c7d30450d048725f65e
dylanfisher <hi@dylanfisher.com>
2016-12-19 00:24:08 -0500

    Think about caching settings

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -7,7 +7,18 @@ class Setting < ApplicationRecord
   scope :by_slug, -> (orderer = :asc) { order(slug: orderer) }
   scope :by_created_at, -> (orderer = :desc) { order(created_at: orderer) }
 
+  def self.for(title)
+    self.settings.friendly.find title.to_s
+  end
+
   private
+
+    def self.settings
+      # TODO: settings cache_key
+      Rails.cache.fetch('settings', expires_in: 1.hour) do
+        Setting.all
+      end
+    end
 
     def should_generate_new_friendly_id?
       slug.blank?
