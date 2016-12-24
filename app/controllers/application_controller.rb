9d2889b22cc0ab554b47e86ac0654bcd813d34d4
dylanfisher <hi@dylanfisher.com>
2016-12-24 13:10:06 -0800

    Unify devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :set_html_classes, :set_page_title
+  before_action :set_body_classes, :set_page_title
   before_action :authentication_check
 
   private
 
-    def set_html_classes
+    def set_body_classes
       classes = []
       classes << "controller--#{controller_name}"
       classes << "action--#{action_name}"
-      @html_classes = classes.join ' '
+      @body_classes = classes.join ' '
     end
 
     def set_page_title
