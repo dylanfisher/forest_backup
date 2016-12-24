04ec3cfa202d43b05a64df11f13792872be1d5ba
dylanfisher <hi@dylanfisher.com>
2016-12-23 18:53:52 -0800

    Tweak settings

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -40,7 +40,7 @@ class SettingsController < ApplicationController
   # PATCH/PUT /settings/1
   def update
     if @setting.update(setting_params)
-      redirect_to @setting, notice: 'Setting was successfully updated.'
+      redirect_to settings_path, notice: 'Setting was successfully updated.'
     else
       render :edit
     end
